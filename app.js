ee64d4d5ae98b20be4b9535b4b4ad735dd9225ea
Tony <tony.inuma@gmail.com>
2022-08-26 15:23:02 -0500

    Feat: implements cookies

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -22,3 +24,17 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useCre
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// Cookies
+app.get('/set-cookies', (req, res) => {
+    res.cookie('newUser', false);
+    res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+
+    res.send('You got the cookies');
+});
+
+app.get('/read-cookies', (req, res) => {
+    const {cookies} = req;
+    console.log(cookies);
+    res.json(cookies);
+});
