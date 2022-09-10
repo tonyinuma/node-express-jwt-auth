6c33faa05dbfe900b8d7cab3676cd922f5a62f64
Tony <tony.inuma@gmail.com>
2022-09-10 16:46:46 -0500

    Add: Signup function to register a new user

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,7 +47,9 @@ authController.signupPost = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.create({ email, password })
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
     } catch (err) {
         const errors = handleErrors(err)
         res.status(400).json({ errors });
