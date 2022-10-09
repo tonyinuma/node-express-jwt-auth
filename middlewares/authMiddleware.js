62d75acd27f4413ceb12e3cb6e7200c902ead1ea
Tony <tony.inuma@gmail.com>
2022-10-08 23:47:06 -0500

    Add: Middlewares Folder and Auth Middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookie.jwt;
+
+    if (token) {
+        jwt.verify(token, 'secret_id', (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/login');
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = { requireAuth };
