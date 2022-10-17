1e39ecd5ce5611e89b0dec0b1a9219d6ae82d048
Tony <tony.inuma@gmail.com>
2022-10-17 10:47:17 -0500

    Fix: cookies property name

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const requireAuth = (req, res, next) => {
-    const token = req.cookie?.jwt;
-
+    const token = req.cookies.jwt;
     if (token) {
         jwt.verify(token, 'secret_id', (err, decodedToken) => {
             if (err) {
